6233de6a4673c342bafaab13e4e7718c5caa8c16
Mary Dick <maryclairedick@gmail.com>
2020-07-17 20:07:04 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,9 +1,9 @@
 # code your #valid_move? method here
 def valid_move?(board, move)
-  if position_taken? == true
-    return false
-  elsif move >= 0 || move <= 8
+  if move >= 0 || move <= 8
     return true
+  if position_taken?
+    return false
   else
     return false
   end
